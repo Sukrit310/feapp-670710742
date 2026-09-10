ea931e4d6491978a3795d0d90e6eda0e566e417a
Sukrit Prasopkeaw <prasopkeaw_s@su.ac.th>
2026-09-10 05:30:38 +0000

    New data and state

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 // import {Link} from 'react-router-dom';
 import { movies } from "../data/data";
 import MovieCard from "../components/MovieCard";
