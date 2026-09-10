d3b8345773dc0a8dfe41ee5eb2c93595114e5078
Sukrit Prasopkeaw <prasopkeaw_s@su.ac.th>
2026-09-10 05:23:10 +0000

    New data and state

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import { movies } from "../data/data";
+import MovieCard from "../components/MovieCard";
+import MovieGallery from "../components/MovieGallery";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800"></h2>
+      <div className="mx-auto mt-8 max-w-5xl">
+        {movies.slice(0, 3).map(m => (
+          <MovieCard key={m.id} title={m.title} year={m.year} />
+        ))}
+        {/* <MovieCard title="INThe Mandalorian and Grogu" year={2026} /> */}
+      </div>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800"></h2>
+      <div className="mx-auto mt-8 max-w-md">
+        <MovieGallery />
+      </div>
     </div>
   );
 }
